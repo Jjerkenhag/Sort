9ceade346c5f4654d042b765d1ee55ade7b6bab1
Fastpole <albin_ae@hotmail.com>
2015-04-09 22:55:23 +0200

    is fix most work kinda

--- Sort/AAA.cpp
+++ Sort/AAA.cpp
@@ -10,27 +10,22 @@ using namespace std;
 
 void sortThread(int x, int off, vector<int> *arr)
 {
-	this_thread::sleep_for(chrono::milliseconds(1000+ x+off));
+	this_thread::sleep_for(chrono::milliseconds(10000+x+off));
 	cout << x << " ";											//skriv da trmellanslag
 	arr->push_back(x);
-	 //return x;
 }
 
-int main()
+void AAAsort(vector<int> &other, int computerbadness)
 {
-	const int computerbadness = 6;
-	vector<thread*> threads;	//da tråds
-	vector<int> arr;
-	srand(time(NULL));											
-	cout << "dram swat mah trehds:" << endl;
+	vector<thread*> threads;	//da tråds										
+	cout << endl << "dram swat mah trehds:" << endl;
 	const int start = GetTickCount();
+	vector<int> arr;
 
-	for (int i = 0; i < 1000; i++)								// make da tråds
+	for (int i = 0; i < other.size(); i++)								// make da tråds
 	{
-		int r = rand() % 10000 + 1;
 		int off = start - GetTickCount();
-		cout << r << " ";
-		thread* t = new thread(sortThread, r, off, &arr);					
+		thread* t = new thread(sortThread, other[i], off, &arr);					
 		threads.push_back(t);
 	}
 	cout << endl << endl << "shit is waking: " << endl;
@@ -41,10 +36,8 @@ int main()
 		delete (*t);
 	}
 
-
-
-
 	cout << "Accounting for the badnessss :"<< endl;
+	int nrofswaps = 0;
 	for (int j = 0; j < computerbadness; j++)
 	{
 		for (int i = 1; i < arr.size(); i++)
@@ -52,29 +45,10 @@ int main()
 			if (arr[i] < arr[i - 1])
 			{
 				swap(arr[i], arr[i - 1]);
+				nrofswaps++;
 			}
 		}
 	}
-	for (int i = 0; i < arr.size(); i++)
-	{
-		cout << arr[i] << " ";
-	}
-	bool issort = true;
-	for (int i = 1; i < arr.size(); i++)
-	{
-		if (arr[i - 1] > arr[i])
-		{
-			issort = false;
-		}
-	}
-	if (issort)
-	{
-		cout << endl << "niec" << endl;
-	}
-	else
-	{
-		cout << "bajs";
-	}
-	getchar();
-	return 0;
+	other = arr;
+	cout << "corrections: " << nrofswaps;
 }
