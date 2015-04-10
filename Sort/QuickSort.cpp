4819e3bbec3b2fa824e64ace6b2dcca412211601
Fritjof Cavallin <fritjof.cavallin@gmail.com>
2015-04-10 23:18:47 +0200

    new quicksort version

--- Sort/QuickSort.cpp
+++ Sort/QuickSort.cpp
@@ -1,17 +1,43 @@
+#include <iostream>
+
 #include "QuickSort.h"
 
-int QuickSort::index;
+int QuickSort::mIndex;
 vector<int> QuickSort::sortList;
+int* QuickSort::list1;
+int* QuickSort::list2;
+bool QuickSort::list1sTurn;
+int QuickSort::size;
 
 //Sorts the vector with quicksort
 void QuickSort::Sort(vector<int>& list)
 {
-	index = 0;
-	sortList.resize(list.size());
-	sortList = list;
-	actualSort(sortList);
-	list = sortList;
-} 
+	//mIndex = 0;
+	//sortList.resize(list.size());
+	//sortList = list;
+	//actualSort(sortList);
+	//list = sortList;
+
+	size = list.size();
+	list1 = new int[list.size()];
+	list2 = new int[list.size()];
+	for (int i = 0; i < list.size(); i++)
+	{
+		list1[i] = list[i];
+		//list2[i] = list[i];
+	}
+	list1sTurn = true;
+	actualSort2(0, list.size() - 1);
+	for (int i = 0; i < list.size(); i++)
+	{
+		if (list1sTurn)
+			list[i] = list2[i];
+		else
+			list[i] = list1[i];
+	}
+	delete[] list1;
+	delete[] list2; 
+}
 
 void QuickSort::actualSort(vector<int> list)
 {
@@ -29,19 +55,93 @@ void QuickSort::actualSort(vector<int> list)
 		actualSort(sub1);
 	else if (sub1.size() == 1)
 	{
-		sortList.erase(sortList.begin() + index);
-		sortList.insert(sortList.begin() + index, sub1[0]);
-		index++;
+		sortList.erase(sortList.begin() + mIndex);
+		sortList.insert(sortList.begin() + mIndex, sub1[0]);
+		mIndex++;
 	}
-	sortList.erase(sortList.begin() + index);
-	sortList.insert(sortList.begin() + index, pivot);
-	index++;
+	sortList.erase(sortList.begin() + mIndex);
+	sortList.insert(sortList.begin() + mIndex, pivot);
+	mIndex++;
 	if (sub2.size() > 1)
 		actualSort(sub2);
 	else if (sub2.size() == 1)
 	{
-		sortList.erase(sortList.begin() + index);
-		sortList.insert(sortList.begin() + index, sub2[0]);
-		index++;
+		sortList.erase(sortList.begin() + mIndex);
+		sortList.insert(sortList.begin() + mIndex, sub2[0]);
+		mIndex++;
 	}
+}
+
+void QuickSort::actualSort2(int startIndex, int endIndex)
+{
+	int lesserListSize = 0;
+	int biggerListSize = 0;
+	int pivotIndex = startIndex + (endIndex - startIndex) / 2;
+	int pivot;
+	int freeSpot = pivotIndex;
+	if (list1sTurn)
+	{
+		pivot = list1[pivotIndex];
+		for (int i = startIndex; i <= endIndex; i++)
+		{
+			int pos;
+			if (i != pivotIndex)
+			{
+				if (list1[i] <= pivot)
+				{
+					pos = startIndex + lesserListSize;
+					list2[pos] = list1[i];
+					lesserListSize++;
+					if (pos == freeSpot)
+						freeSpot++;
+				}
+				else
+				{
+					pos = endIndex - biggerListSize;
+					list2[pos] = list1[i];
+					biggerListSize++;
+					if (pos == freeSpot)
+						freeSpot--;
+				}
+			}
+		}
+		list2[freeSpot] = pivot;
+	}
+	//else
+	//{
+	//	pivot = list2[pivotIndex];
+	//	for (int i = startIndex; i <= endIndex; i++)
+	//	{
+	//		int pos;
+	//		if (i != pivotIndex)
+	//		{
+	//			if (list2[i] <= pivot)
+	//			{
+	//				pos = startIndex + lesserListSize;
+	//				list1[pos] = list2[i];
+	//				lesserListSize++;
+	//				if (pos == freeSpot)
+	//					freeSpot++;
+	//			}
+	//			else
+	//			{
+	//				pos = endIndex - biggerListSize;
+	//				list1[pos] = list2[i];
+	//				biggerListSize++;
+	//				if (pos == freeSpot)
+	//					freeSpot--;
+	//			}
+	//		}
+	//	}
+	//	list1[freeSpot] = pivot;
+	//}
+	for (int i = 0; i < size; i++)
+	{
+		list1[i] = list2[i];
+	}
+	//list1sTurn = !list1sTurn;
+	if (lesserListSize > 1)
+		actualSort2(startIndex, startIndex + lesserListSize - 1);
+	if (biggerListSize > 1)
+		actualSort2(endIndex - biggerListSize + 1, endIndex);
 }
--- Sort/QuickSort.h
+++ Sort/QuickSort.h
@@ -8,10 +8,15 @@ using namespace std;
 class QuickSort
 {
 private:
-	static int index;
+	static int mIndex;
 	static vector<int> sortList;
+	static int* list1;
+	static int* list2;
+	static bool list1sTurn;
+	static int size;
 
 	static void actualSort(vector<int> list);
+	static void actualSort2(int startIndex, int endIndex);
 
 public:
 	static void Sort(vector<int>& list);
